1bdea99b10ae0c29effa10fa4743967228710a74
MAPLE <yuechuyc@foxmail.com>
2023-10-07 15:30:01 +0800

    Update MAPLETOOLS.js
    optimize the code

--- MAPLETOOLS.js
+++ MAPLETOOLS.js
@@ -4,6 +4,8 @@ import fs from 'node:fs'
 import yaml from 'yaml'
 import path from 'path'
 
+var blackListTriggered = false
+
 const _path = process.cwd()
 const file = './config/config/qq.yaml'
 const Bot = yaml.parse(fs.readFileSync(file, 'utf8'))
@@ -11,7 +13,7 @@ const Bot = yaml.parse(fs.readFileSync(file, 'utf8'))
 const cacheDirs = [{
     name: 'data/image/',
     path: `${_path}/data/icqq/${Bot.qq}/image/`,
-    clearReg: /^[a-z0-9]{32}$/,
+    clearReg: /^[a-z0-9]{8}|[a-z0-9]{16}|[a-z0-9]{32}$/,
 }]
 
 /** 开启Bot提示 */
@@ -19,8 +21,6 @@ const ON_TIP = '你好，你好，大家好'
 
 /** 关闭Bot提示 */
 const CLOSE_TIP = '祝你拥有愉快的一天，再见'
-
-let blackListTriggered = false
 
 export class MAPLETOOLS extends plugin {
     constructor() {
@@ -35,40 +35,40 @@ export class MAPLETOOLS extends plugin {
             priority: 0,
             rule: [
                 {
-                    reg: '^字符串搜索(.*)$', //在云崽根目录下遍历所有文件查找指定字符串
+                    reg: '^#?字符串搜索(.*)$', //在云崽根目录下遍历所有文件查找指定字符串
                     fnc: 'SearchFiles',
                 },
                 {
-                    reg: '^(今日)?(日报|新闻)$',
+                    reg: '^#?(今日)?(日报|新闻)$',
                     fnc: 'News',
                 },
                 {
-                    reg: '^(加|拉)黑(名单)?(.*)$',
+                    reg: '^#?(加|拉)黑(名单)?(.*)$',
                     fnc: 'addBlackList',
                     permission: 'master',
                 },
                 {
-                    reg: '^(查看)?黑名单(列表)?$',
+                    reg: '^#?(查看)?黑名单(列表)?$',
                     fnc: 'BlackList',
                     permission: 'master',
                 },
                 {
-                    reg: '^删(除)?黑(名单)?(.*)$',
+                    reg: '^#?删(除)?黑(名单)?(.*)$',
                     fnc: 'delBlackList',
                     permission: 'master',
                 },
                 {
-                    reg: '^(启动|开启)机器人$',
+                    reg: '^#?(启动|开启)机器人$',
                     fnc: 'Power_ON',
                     permission: 'master',
                 },
                 {
-                    reg: '^关闭机器人$',
+                    reg: '^#?关闭机器人$',
                     fnc: 'Power_Off',
                     permission: 'master',
                 },
                 {
-                    reg: '^清理缓存文件$',
+                    reg: '^#?清理缓存文件$',
                     fnc: 'clearCache',
                     permission: 'master',
                 }
@@ -79,7 +79,7 @@ export class MAPLETOOLS extends plugin {
     /** SearchFiles */
     async SearchFiles(e) {
         if (e.isGroup) return e.reply('[WARN] 请私聊使用！')
-        let searchString = e.msg.match(/^字符串搜索(.*)$/)?.[1]
+        const searchString = e.msg.match(/^#?字符串搜索(.*)$/)?.[1]
         if (searchString) {
             await e.reply('[WAIT] 正在搜索：' + searchString)
 
@@ -103,31 +103,25 @@ export class MAPLETOOLS extends plugin {
 
     /** 日报 */
     async News(e) {
-        if (e.isGroup) {
-            return e.reply(segment.image('https://api.03c3.cn/zb'), true)
-        } else {
-            return e.reply(segment.image('https://api.03c3.cn/zb'))
-        }
+        return e.reply(segment.image('https://api.03c3.cn/zb'), e.isGroup ? true : false)
     }
 
     /** 加黑名单 */
     async addBlackList(e) {
         if (!e.atBot || e.isGroup) {
             if ((e.message[0] && e.message[0].type == 'at') || (e.message[1] && e.message[1].type == 'at')) {
-                let config = getConfig()
+                const config = getConfig()
                 if (config.masterQQ.includes(e.at)) return e.reply('？')
-                let Memberinfo = e.group.pickMember(Number(e.at)).info
+                const Memberinfo = e.group.pickMember(Number(e.at)).info
                 if (config.blackQQ.includes(e.at)) return e.reply(`已存在黑名单：\n${Memberinfo.nickname}(${e.at})`)
 
                 addToBlackList(config, e.at)
                 return e.reply(`已将${Memberinfo.nickname}(${e.at})拉黑！`)
             }
         }
-
-        let QQNumber = e.msg.replace(new RegExp(`^(加|拉)黑(名单)?(.*)$`), '$3')
-        QQNumber = QQNumber.replace(/ /g, '')
+        const QQNumber = e.msg.replace(new RegExp(`^#?(加|拉)黑(名单)?(.*)$`), '$3').replace(/ /g, '')
         if (Number.isInteger(Number(QQNumber)) && QQNumber.length >= 5 && QQNumber.length <= 10) {
-            let config = getConfig()
+            const config = getConfig()
             // 检查输入的 QQ 号是否是主人
             if (config.masterQQ.includes(Number(QQNumber))) return e.reply('？')
             if (config.blackQQ.includes(Number(QQNumber))) return e.reply(`已存在黑名单：${QQNumber}`)
@@ -140,23 +134,15 @@ export class MAPLETOOLS extends plugin {
 
     /** 黑名单 */
     async BlackList(e) {
-        let config = getConfig()
-        let blackQQList = config.blackQQ
-        if (blackQQList.length === 0) {
-            return e.reply('黑名单为空！')
-        }
+        const config = getConfig()
+        const blackQQList = config.blackQQ
+        if (blackQQList.length === 0) return e.reply('黑名单为空！')
         let replyStr = ''
         if (e.isGroup) {
             // 在群组内触发指令
             for (let i = 0; i < blackQQList.length; i++) {
-                let member = e.group.pickMember(blackQQList[i]).info
-                if (member) {
-                    // 成员在当前群组中
-                    replyStr += `${i + 1}-${member.nickname}(${blackQQList[i]})\n`
-                } else {
-                    // 成员不在当前群组中
-                    replyStr += `${i + 1}-${blackQQList[i]}\n`
-                }
+                const member = e.group.pickMember(blackQQList[i]).info
+                replyStr += member ? `${i + 1}-${member.nickname}(${blackQQList[i]})\n` : `${i + 1}-${blackQQList[i]}\n`
             }
         } else {
             // 在私聊中触发指令
@@ -164,7 +150,7 @@ export class MAPLETOOLS extends plugin {
                 replyStr += `${i + 1}-${blackQQList[i]}\n`
             }
         }
-        let msgArray = ['可使用“删黑+序号”取消拉黑', replyStr]
+        const msgArray = ['可使用“删黑+序号”取消拉黑', replyStr]
         blackListTriggered = true
         return e.reply(await common.makeForwardMsg(e, msgArray, '[-----黑名单-----]'))
     }
@@ -172,66 +158,52 @@ export class MAPLETOOLS extends plugin {
     /** 删黑名单 */
     async delBlackList(e) {
         if (!blackListTriggered) return e.reply('[WARN] 请先查看黑名单列表再执行删除操作！')
-        let index = e.msg.replace(new RegExp(`^删(除)?黑(名单)?(.*)$`), '$3')
+        let index = e.msg.replace(new RegExp(`^#?删(除)?黑(名单)?(.*)$`), '$3')
         if (index.trim() === '' || index.trim() === null) return false
         index = parseInt(index)
 
-        let config = getConfig()
-        let blackQQList = config.blackQQ
+        const config = getConfig()
+        const blackQQList = config.blackQQ
         if (isNaN(index) || index < 1 || index > blackQQList.length) return e.reply('[WARN] 请输入正确的序号！')
 
-        let deletedQQ = blackQQList.splice(index - 1, 1)
+        const deletedQQ = blackQQList.splice(index - 1, 1)
         writeConfig(config)
         return e.reply(`已取消拉黑：${deletedQQ}`)
     }
 
     /** 开机 */
     async Power_ON(e) {
-        if (e.isGroup) {
-            this.file = './config/config/group.yaml'
-            let data = yaml.parse(await fs.promises.readFile(this.file, 'UTF8'))
-            data[e.group_id] = { enable: null }
-            let newyaml = yaml.stringify(data)
-            await fs.promises.writeFile(this.file, newyaml, 'UTF8')
-            return e.reply(ON_TIP)
-        } else {
-            return e.reply('[WARN]请在群聊中使用！')
-        }
+        if (!e.isGroup) return e.reply('[WARN]请在群聊中使用！')
+        const file = './config/config/group.yaml'
+        const data = yaml.parse(await fs.promises.readFile(file, 'UTF8'))
+        data[e.group_id] = { enable: null }
+        const newyaml = yaml.stringify(data)
+        await fs.promises.writeFile(file, newyaml, 'UTF8')
+        return e.reply(ON_TIP)
     }
 
     /** 关机 */
     async Power_Off(e) {
-        if (e.isGroup) {
-            this.file = './config/config/group.yaml'
-            let data = yaml.parse(await fs.promises.readFile(this.file, 'UTF8'))
-            data[e.group_id] = { enable: ['MAPLETOOLS',] }
-            let newYaml = yaml.stringify(data)
-            await fs.promises.writeFile(this.file, newYaml, 'UTF8')
-            return e.reply(CLOSE_TIP)
-        } else {
-            return e.reply('[WARN]请在群聊中使用！')
-        }
+        if (!e.isGroup) return e.reply('[WARN]请在群聊中使用！')
+        const file = './config/config/group.yaml'
+        const data = yaml.parse(await fs.promises.readFile(file, 'UTF8'))
+        data[e.group_id] = { enable: ['MAPLETOOLS',] }
+        const newYaml = yaml.stringify(data)
+        await fs.promises.writeFile(file, newYaml, 'UTF8')
+        return e.reply(CLOSE_TIP)
     }
 
     /** 清理缓存文件 */
     async clearCache(e) {
         let dataCount = 0
-        try {
-            await Promise.all(cacheDirs.map(async (dirItem) => {
-                const cachefiles = await fs.promises.readdir(dirItem.path)
-                await e.reply(`开始清理${dirItem.name}缓存文件…`)
-                await Promise.all(cachefiles.map(async (file) => {
-                    if (new RegExp(dirItem.clearReg).test(file)) {
-                        await fs.promises.unlink(path.join(dirItem.path, file))
-                        dataCount++
-                    }
-                }))
-            }))
-            return e.reply(`[Success] 清理完成，共清理缓存文件：${dataCount}个！`)
-        } catch (err) {
-            logger.warn(err)
-            return false
-        }
+        await Promise.all(cacheDirs.map(async (dirItem) => {
+            await e.reply(`开始清理${dirItem.name}缓存文件…`)
+            const cacheFiles = await fs.promises.readdir(dirItem.path, { withFileTypes: true })
+            const filesToClear = cacheFiles.filter(file => new RegExp(dirItem.clearReg).test(file.name))
+            await Promise.all(filesToClear.map(file => fs.promises.unlink(path.join(dirItem.path, file.name))))
+            dataCount += filesToClear.length
+        }))
+        return e.reply(`[Success] 清理完成，共清理缓存文件：${dataCount}个！`)
     }
 }
 
@@ -243,7 +215,7 @@ function ReadYaml(filePath) {
 
 function getConfig() {
     // 读取yaml文件
-    let config = readConfig()
+    const config = readConfig()
     if (!Array.isArray(config.blackQQ)) {
         config.blackQQ = []
     }
@@ -259,11 +231,7 @@ function getConfig() {
 function addToBlackList(config, qq) {
     config.blackQQ.push(qq)
     // 写入配置文件
-    try {
-        writeConfig(config)
-    } catch (error) {
-        logger.warn(error)
-    }
+    writeConfig(config)
 }
 
 function readConfig() {
@@ -303,7 +271,6 @@ async function searchInFiles(folderPath, searchString) {
             logger.warn(`Error while processing ${currentPath}: ${err.message}`)
         }
     }
-
     return results
 }
 
